7b35dc4f6031b57ef4fd3792a1176b63385fcc97
jelena2108 <75537969+jelena2108@users.noreply.github.com>
2021-02-17 23:57:33 +0100

    Update niz.php

--- niz.php
+++ niz.php
@@ -2,14 +2,35 @@
 $arr[0]=array(1,"Peter","Andersen","peter@gmail.com");
 $arr[1]=array(2,"John","Miller","john@gmail.com");
 $arr[2]=array(3,"Thomas","Swift","thomas@gmail.com");
+$arr[3]=array(4,"Ema","Cruise","ema@gmail.com");
 
- for($i=0;$i<count($arr);$i++){
-    
-     for($u=0;$u<count($arr[$i]);$u++){
-      
-         echo $arr[$i][$u];
-         echo "<br>";
-     }
- }
-
+  
 ?>
+<html>
+    <head></head>
+    <body>
+        <table style="border:1px solid black;width:400px">
+            
+    <?php
+
+    for($i=0;$i<count($arr);$i++){
+    ?>
+        <tr>
+    <?php 
+       for($u=0;$u<count($arr[$i]);$u++){
+    ?>
+         <td style="border:1px solid black">
+    <?php echo $arr[$i][$u];
+    ?> 
+         </td>
+    <?php  
+     } 
+    ?>
+         </tr>
+   <?php 
+     
+    }
+       ?>
+        </table>
+    </body>
+</html>
